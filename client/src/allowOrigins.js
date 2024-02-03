aee66f3d76ba3883fa4df43586514d356570ab47
ahmed <ahmedmhmouad41@gmail.com>
2024-02-04 01:20:24 +0200

    commit

--- client/src/allowOrigins.js
+++ client/src/allowOrigins.js
@@ -1,7 +1,7 @@
 const allowOrigins={
     "render" :"https://sociopedia-djft.onrender.com",
     "local" : "http://localhost:3001",
-    "netlify":"https://65b9382c2256671de5ab39f6--dynamic-kataifi-d20fad.netlify.app"
+    "netlify":"https://master--cerulean-cassata-7dc972.netlify.app"
 }
 
 export default allowOrigins
